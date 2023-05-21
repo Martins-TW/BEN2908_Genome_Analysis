01a41ecd841912654b4adaf6b5e04b07a18d42ab
Martins-TW <133924832+Martins-TW@users.noreply.github.com>
2023-05-21 16:42:40 -0300

    1.OG_Extractor.py

--- 1.OG_Extractor.py
+++ 1.OG_Extractor.py
@@ -13,12 +13,12 @@ OGs = [] #list with SingleCopyOthogroups
 lst_sog= [] #list of lists with the OGXXXXXXXXX and the group of genes to be aligned in
 
 
-with open('adress/adress.txt','r') as single_ortho: #change adress
+with open('address/address.txt','r') as single_ortho: #change address
     for og in single_ortho:
         og = og.split('\n')
         OGs.append(og[0])
 
-with open('adress/adress.csv','r') as Ortho: #change adress
+with open('address/address.csv','r') as Ortho: #change address
     Orthodata = csv.reader(Ortho)
     Orthodata = [[pos-1,row[0]] for pos,row in enumerate(Orthodata)]
     Orthodata.pop(0)
@@ -36,7 +36,7 @@ for sog in lst_sog:
             with open ('APECO1.faa','r') as apeco1:
                 for record in SeqIO.parse(apeco1, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -47,7 +47,7 @@ for sog in lst_sog:
             with open ('BEN2908.faa','r') as ben2908:
                 for record in SeqIO.parse(ben2908, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -58,7 +58,7 @@ for sog in lst_sog:
             with open ('CFT073.faa','r') as cft073:
                 for record in SeqIO.parse(cft073, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -69,7 +69,7 @@ for sog in lst_sog:
             with open ('chi7122.faa','r') as chi7122:
                 for record in SeqIO.parse(chi7122, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -80,7 +80,7 @@ for sog in lst_sog:
             with open ('IMT5155.faa','r') as imt5155:
                 for record in SeqIO.parse(imt5155, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -91,7 +91,7 @@ for sog in lst_sog:
             with open ('K12.faa','r') as k12:
                 for record in SeqIO.parse(k12, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -102,7 +102,7 @@ for sog in lst_sog:
             with open ('LF82.faa','r') as lf82:
                 for record in SeqIO.parse(lf82, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -113,7 +113,7 @@ for sog in lst_sog:
             with open ('IHE3034.faa','r') as ihe3034:
                 for record in SeqIO.parse(ihe3034, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -124,7 +124,7 @@ for sog in lst_sog:
             with open ('NRG857c.faa','r') as nrg857c:
                 for record in SeqIO.parse(nrg857c, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -135,7 +135,7 @@ for sog in lst_sog:
             with open ('RS218.faa','r') as rs218:
                 for record in SeqIO.parse(rs218, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -146,7 +146,7 @@ for sog in lst_sog:
             with open ('SCI07.faa','r') as sci07:
                 for record in SeqIO.parse(sci07, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -157,7 +157,7 @@ for sog in lst_sog:
             with open ('SCU-397.faa','r') as scu397:
                 for record in SeqIO.parse(scu397, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -168,7 +168,7 @@ for sog in lst_sog:
             with open ('PSUO2.faa','r') as psuo2:
                 for record in SeqIO.parse(psuo2, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
@@ -176,10 +176,10 @@ for sog in lst_sog:
                                                    record.description)
                             SeqIO.write(nrecord, file_sog, "fasta")
         elif val_cepa == 871102:
-            with open ('78-Pyelo.faa','r') as _78pyelo: #Na verdade é 78-pyelo a linhagem
+            with open ('78-Pyelo.faa','r') as _78pyelo:
                 for record in SeqIO.parse(_78pyelo, "fasta"):
                     if figpeg == record.id:
-                        with open ('Muscle_input/' + num_sog + '.fasta', 'a') as file_sog:
+                        with open ('address/' + num_sog + '.fasta', 'a') as file_sog: #change address
                             lst_record_id= record.id.split('.')
                             nrecord= SeqRecord(Seq (str(record.seq)),
                                                    lst_record_id[1] + lst_record_id[3],
