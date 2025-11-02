f3825b232dd794e06abfa46f9d66d5489aeb0c44
Martins-TW <133924832+Martins-TW@users.noreply.github.com>
2025-11-02 19:14:36 -0300

    Delete 4.afa_to_phy.py

--- 4.afa_to_phy.py
+++
@@ -1,9 +0,0 @@
-import os
-from Bio import SeqIO 
-""" Input: NameOfTheStrain.fasta (merged multifasta file with all the outputs from afa_conatenator)
-    Output: NameOfTheStrain.phy (a .phylip file for RAxML input)"""
-
-afa_records = SeqIO.parse("address/address/File.txt","fasta") #change address
-phy_records = SeqIO.write(afa_records, "address/address/File.phy", "phylip") #change address
-
-print ("\ndone")
